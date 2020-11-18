2fbe82b25690b3fa556cd3cfd252bfbdd5c0fc64
Naum Tuntev <tuntev@gmail.com>
2020-11-18 12:50:16 +0100

    Added terms modal. Redesigned the footer. Added responsive hiding of the side-bar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss']
 })
+
 export class AppComponent {
-  title = 'nail-polish';
+
+    title = 'nail-polish';
+
+    openTermsModal(){
+        // $('#myModal').modal('show')
+    }
+
+    openPrivacyPolicy(){
+        console.log('privacy');
+
+    }
 }
